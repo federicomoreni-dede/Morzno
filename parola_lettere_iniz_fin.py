46c182c54ed7ff1fcba61c39e9658f98cac3d46c
MoreniFederico <federico.moreni@liceoeinsteinmilano.edu.it>
2025-10-21 13:13:39 +0200

    esercizio 8 selezione (file) 17 ottobre classroom

---
+++ parola_lettere_iniz_fin.py
@@ -0,0 +1,6 @@
+#parola_esercizio_lettere_iniziali_finali
+parola=input("inserisci un parola: ")
+if parola[0]==parola[-1]:
+    print("lettere iniziali e finali uguali")
+else:
+    print("lettere iniziali e finali diverse")
