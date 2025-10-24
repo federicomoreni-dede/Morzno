32bbdd25d025df9b2a0cf5d417d43f94e1fd173e
MoreniFederico <federico.moreni@liceoeinsteinmilano.edu.it>
2025-10-24 13:10:15 +0200

    aggiunti esercizi

---
+++ #area esagono.py
@@ -0,0 +1,7 @@
+#area esagono 
+lato=float(input("inserisci il lato:"))
+perimetro=lato*6
+print(perimetro)
+apotema=(lato/2)*1,732050807568877
+area=perimetro*apotema/2
+print(area)
---
+++ #esercizio numero_python.py
@@ -0,0 +1,7 @@
+#esercizio numero pari
+numero=int(input("inserisci un numero qualsiasi:"))
+if ((numero%2)==0):
+    print("il numero è pari")
+else:
+    print("il numero è dispari")
+    
