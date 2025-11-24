ef313dc0030fc3bebf73c6558b990ef37b125f74
federicomoreni-dede <federico.moreni@liceoeinsteinmilano.edu.it>
2025-11-24 14:48:05 +0100

    commit esercizio 21

---
+++ esericizio21.py
@@ -0,0 +1,9 @@
+#esercizio 21
+totnum=int(input("inserisci un numero:"))
+conta=0
+somma=0
+while conta<totnum:
+    prezzo=float(input("inserisci un prezzo"))
+    conta+=1
+    somma=somma+prezzo
+print(somma)
