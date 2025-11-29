507adb4c4f89962806f6f36ef37f7cebbfd759a6
federicomoreni-dede <federico.moreni@liceoeinsteinmilano.edu.it>
2025-11-29 15:48:31 +0100

    aggiunto esercizio ciclo 2-12-25

---
+++ esClassroom2-12-25.py
@@ -0,0 +1,14 @@
+#esercizio_coding_2/12/25
+stop=int(input("inserisci un numero di stop"))
+n=int(input("inserisci un numero:"))
+if stop<=0:
+    print("non può inserire valori negativi")
+else:
+    conta=1
+    massimo=n
+    while conta<stop:             
+        n=int(input("inserisci un numero:"))
+        conta+=1
+        if n>massimo:
+            massimo=n
+print(massimo)
